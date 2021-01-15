3dd68119372a85ff118cb05c386eb420223aaa39
Kaustubh Padwal <11kaustubhpadwal@gmail.com>
2021-01-15 23:11:21 +0100

    Routing and UI library setup done.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import Home from "./routes/home/Home";
+import About from "./routes/about/About";
+import Admin from "./routes/admin/Admin";
+import Profile from "./routes/profile/Profile";
+import PageNotFound from "./routes/404/PageNotFound";
+import Navbar from "./shared components/Navbar";
+import Footer from "./shared components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/about">
+          <Navbar />
+          <About />
+          <Footer />
+        </Route>
+        <Route exact path="/profile">
+          <Navbar />
+          <Profile />
+          <Footer />
+        </Route>
+        <Route exact path="/admin">
+          <Navbar />
+          <Admin />
+          <Footer />
+        </Route>
+        <Route exact path="/">
+          <Navbar />
+          <Home />
+          <Footer />
+        </Route>
+        <Route exact path="*">
+          <Navbar />
+          <PageNotFound />
+          <Footer />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
